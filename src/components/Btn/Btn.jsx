b13015007f9854779b8ae8c7c81bbc27220f9472
dev-rioma <dev.rioma@gmail.com>
2023-04-28 15:26:29 -0500

    change style btn clearfilter

--- src/components/Btn/Btn.jsx
+++ src/components/Btn/Btn.jsx
@@ -1,11 +1,10 @@
-import { Button } from "@mui/material";
 import "./Btn.css";
 
 const Btn = ({ handleClearFilter, title }) => {
   return (
-    <Button className="hover" variant="contained" onClick={handleClearFilter}>
+    <button className="hover" onClick={handleClearFilter}>
       {title}
-    </Button>
+    </button>
   );
 };
 
